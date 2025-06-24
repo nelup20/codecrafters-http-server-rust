382da9e784a5e372a51499da7814c09edb03bbf5
Nelu Platonov <nelu.platonov@gmail.com>
2025-06-24 03:33:10 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,39 @@ mod compression_scheme;
 use crate::request::handle_connection;
 use std::net::TcpListener;
 use std::{env, thread};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let mut args = env::args();
     let mut dir_arg = String::new();
-    
+
     for arg in args.by_ref() {
         if arg == "--directory" {
             dir_arg = args.next().unwrap();
             break
         }
     }
-    
+
     let base_dir = Arc::new(dir_arg);
-    
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
          match stream {
-             Ok(mut tcp_stream) => {
-                 let file_dir = Arc::clone(&base_dir);
-                 thread::spawn(move || handle_connection(&mut tcp_stream, &file_dir));
+             Ok(tcp_stream) => {
+                 let shared_file_dir = Arc::clone(&base_dir);
+                 
+                 thread::spawn(move || {
+                     let shared_connection = Arc::new(Mutex::new(tcp_stream));
+                     
+                     loop {
+                         let file_dir = Arc::clone(&shared_file_dir);
+                         let connection = Arc::clone(&shared_connection);
+
+                         thread::spawn(move || {
+                             handle_connection(&mut connection.lock().unwrap(), &file_dir);
+                         });
+                     } 
+                 });
              }
              Err(e) => {
                  println!("error: {}", e);
